9f99a8e1771c45dd3dc6866f9bcfb9dbc6a0cced
Alex Woodhouse <alexander.woodhouse@perfect.com>
2017-12-11 17:33:07 -0500

    String to int.

--- qatools-service/src/main/java/com/qatools/Main.java
+++ qatools-service/src/main/java/com/qatools/Main.java
@@ -38,8 +38,8 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        String input = "";
-	while(!input.equals("EXIT")) {
+        int input = 999;
+	while(!input != 999999) {
 		input = System.in.read();
 	}
         server.stop();
